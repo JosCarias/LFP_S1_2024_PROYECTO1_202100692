f906efdc1274422b7546092c886dd48089559434
Josue Ricardo Carias Ordoñez <3661044620101@ingenieria.usac.edu.gt>
2024-03-27 13:44:28 -0600

    Va la mitad par generar el html

--- hacerHtml.py
+++ hacerHtml.py
@@ -23,7 +23,9 @@ def hacerHtml():
     for i in range(len(tokens)):
         if tokens[i]=="TituloPagina":
             tituloHead=tokens[i+3]
-    html+=tituloHead+"</title>\n</head>\n<body>\n"
+            
+    html+=tituloHead+"</title>\n</head>\n"
+    
     
     for i in range(len(tokens)):
         if tokens[i] == "Titulo":
@@ -90,7 +92,86 @@ def hacerHtml():
         if tokens[i] == "Salto":
             salto.append(tokens[i+6])
         if tokens[i] == "Tabla":
-            tabla.append(tokens[i+6])
+            j = i + 1
+            atributos = {}  # Diccionario para almacenar los atributos del título de forma dinámica
+            while j < len(tokens) and tokens[j] != "}":
+                # Comprobar si el token actual es un atributo válido del título
+                if tokens[j] in ["filas", "columnas"]:
+                    # Añadir el atributo y su valor al diccionario de atributos
+                    atributos[tokens[j]] = tokens[j + 3]
+                j += 1
+            # Agregar los valores del diccionario a la lista de título
+            tabla.append([atributos.get("filas", ""), 
+                           atributos.get("columnas", "")])
+    
+    for lista in fondo:
+        estilo = ""
+        # Establecer el estilo de acuerdo al color de fondo
+        if lista == "rojo":
+            estilo += 'background-color: red;'
+        elif lista == "amarillo":
+            estilo += 'background-color: yellow;'
+        elif lista == "azul":
+            estilo += 'background-color: blue;'
+        else:
+            estilo += 'background-color: ' + lista + ';'
+        html += '<body style="' + estilo + '">' + '\n'
+    
+     
+    for lista in titulo:
+        estilo = ""
+        posicion = lista[1]
+        tamaño = lista[2]
+        color = lista[3]
+
+        # Establecer el estilo de acuerdo a la posición
+        if posicion == "izquierda":
+            estilo += 'position: absolute; left: 0px;'
+        elif posicion == "derecha":
+            estilo += 'position: absolute; right: 0;'
+        elif posicion == "centro":
+            estilo += 'position: absolute; left: 50%; transform: translateX(-50%);'
+
+        # Establecer el estilo de acuerdo al tamaño
+        if tamaño == "t1":
+            estilo += 'font-size: 30px;'
+        elif tamaño == "t2":
+            estilo += 'font-size: 25px;'
+        elif tamaño == "t3":
+            estilo += 'font-size: 20px;'
+        elif tamaño == "t4":
+            estilo += 'font-size: 15px;'
+        elif tamaño == "t5":
+            estilo += 'font-size: 12px;'
+        elif tamaño == "t6":
+            estilo += 'font-size: 10px;'
+
+        # Establecer el estilo de acuerdo al color
+        if color == "rojo":
+            estilo += 'color: red;'
+        elif color == "amarillo":
+            estilo += 'color: yellow;'
+        elif color == "azul":
+            estilo += 'color: blue;'
+        else:
+            estilo += 'color: ' + color + ';'
+
+        # Agregar el div al HTML con el estilo aplicado
+        html += '   <div style="' + estilo + '">' + lista[0] + '</div>\n'
+    
+    for lista in parrafo:
+        estilo=""
+        texto=lista[0]
+        posicion=lista[1] 
+        # Establecer el estilo de acuerdo a la posición
+        if posicion == "izquierda":
+            estilo += 'position: absolute; left: 0px;'
+        elif posicion == "derecha":
+            estilo += 'position: absolute; right: 0;'
+        elif posicion == "centro":
+            estilo += 'position: absolute; left: 50%; transform: translateX(-50%);' 
+        # Agregar el div al HTML con el estilo aplicado
+        html += '   <p style="' + estilo + '">' + lista[0] + '</p>\n'  
             
                     
     
@@ -105,6 +186,7 @@ def hacerHtml():
     print(tachado)
     print(cursiva)
     print(salto)
+    print(tabla)
      
         
     # Guardamos el HTML en un archivo
