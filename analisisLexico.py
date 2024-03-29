46bc41760019aaf96804aaef4b9f375fd1c2e7e1
Josue Ricardo Carias Ordoñez <3661044620101@ingenieria.usac.edu.gt>
2024-03-29 16:39:19 -0600

    Tabla de errores de token

--- analisisLexico.py
+++ analisisLexico.py
@@ -2,6 +2,8 @@ tokens=[]
 columnas=[]
 filas=[]
 errores=[]
+erColumnas=[]
+erFilas=[]
 palabras_reservadas = ["Inicio", 
                        "Encabezado", 
                        "TituloPagina", 
@@ -47,14 +49,14 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i)) 
-                filas.append(j)      
+                filas.append(str(j))      
         if linea_sin_sangria[0:10] == palabras_reservadas[1] and linea_sin_sangria[10]==":": #Encabezado
             tokens.append(palabras_reservadas[1])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:12] == palabras_reservadas[2] and linea_sin_sangria[12]==":": #TituloPagina
             tokens.append(palabras_reservadas[2])
             tokens.append(linea_sin_sangria[12])  # Agregar el primer carácter después de la coincidencia
@@ -64,21 +66,21 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])  # Agregar el último carácter de la línea
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:6] == palabras_reservadas[3] and linea_sin_sangria[6]==":": #Cuerpo
             tokens.append(palabras_reservadas[3])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:6] == palabras_reservadas[4] and linea_sin_sangria[6]==":":  #Titulo
             tokens.append(palabras_reservadas[4])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[5] and linea_sin_sangria[5]==":":  #texto
             tokens.append(palabras_reservadas[5])
             tokens.append(linea_sin_sangria[5])
@@ -88,7 +90,7 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i)) 
-                filas.append(j)
+                filas.append(str(j))
         if linea_sin_sangria[0:8] == palabras_reservadas[6] and linea_sin_sangria[8]==":":  #posicion
             tokens.append(palabras_reservadas[6])
             tokens.append(linea_sin_sangria[8])
@@ -98,7 +100,7 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i)) 
-                filas.append(j)
+                filas.append(str(j))
         if linea_sin_sangria[0:6] == palabras_reservadas[7] and (linea_sin_sangria[6]==":" or linea_sin_sangria[6]=="="):  #tamaño
             tokens.append(palabras_reservadas[7])
             tokens.append(linea_sin_sangria[6])
@@ -108,7 +110,7 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[8] and (linea_sin_sangria[5]==":" or linea_sin_sangria[5]=="="):  #color
             tokens.append(palabras_reservadas[8])
             tokens.append(linea_sin_sangria[5])
@@ -118,28 +120,28 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[9] and linea_sin_sangria[5]==":":  #Fondo
             tokens.append(palabras_reservadas[9])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:7] == palabras_reservadas[10] and linea_sin_sangria[7]==":":  #Parrafo
             tokens.append(palabras_reservadas[10])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[11] and linea_sin_sangria[5]==":":  #Texto
             tokens.append(palabras_reservadas[11])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:6] == palabras_reservadas[12] and linea_sin_sangria[6]=="=":  #fuente
             tokens.append(palabras_reservadas[12])
             tokens.append(linea_sin_sangria[6])
@@ -149,49 +151,49 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:6] == palabras_reservadas[13] and linea_sin_sangria[6]==":":  #Codigo
             tokens.append(palabras_reservadas[13])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:7] == palabras_reservadas[14] and linea_sin_sangria[7]==":":  #Negrita
             tokens.append(palabras_reservadas[14])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:9] == palabras_reservadas[15] and linea_sin_sangria[9]==":":  #Subrayado
             tokens.append(palabras_reservadas[15])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:7] == palabras_reservadas[16] and linea_sin_sangria[7]==":":  #Tachado
             tokens.append(palabras_reservadas[16])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:7] == palabras_reservadas[17] and linea_sin_sangria[7]==":":  #Cursiva
             tokens.append(palabras_reservadas[17])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[18] and linea_sin_sangria[5]==":":  #Salto
             tokens.append(palabras_reservadas[18])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:8] == palabras_reservadas[19] and linea_sin_sangria[8]==":":  #cantidad
             tokens.append(palabras_reservadas[19])
             tokens.append(linea_sin_sangria[8])
@@ -201,14 +203,14 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[20] and linea_sin_sangria[5]==":":  #Tabla
             tokens.append(palabras_reservadas[20])
             tokens.append(linea_sin_sangria[-2])
             tokens.append(linea_sin_sangria[-1])
             for i in range(3):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:5] == palabras_reservadas[21] and linea_sin_sangria[5]==":":  #filas
             tokens.append(palabras_reservadas[21])
             tokens.append(linea_sin_sangria[5])
@@ -218,7 +220,7 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:8] == palabras_reservadas[22] and linea_sin_sangria[8]==":":  #columna
             tokens.append(palabras_reservadas[22])
             tokens.append(linea_sin_sangria[8])
@@ -228,7 +230,7 @@ def analizador(contenido):
             tokens.append(linea_sin_sangria[-1])
             for i in range(6):
                 columnas.append(str(i))
-                filas.append(j) 
+                filas.append(str(j)) 
         if linea_sin_sangria[0:8] == palabras_reservadas[23] and linea_sin_sangria[8]==":":  #elemento
             tokens.append(palabras_reservadas[23])
             tokens.append(linea_sin_sangria[8])
@@ -270,19 +272,18 @@ def analizador(contenido):
         if linea_sin_sangria[0] == '}':
             tokens.append("}")
             columnas.append(str(i)) 
-            filas.append(j)
+            filas.append(str(j))
         if linea_sin_sangria[0] == "]":
             tokens.append("]")
             columnas.append(str(i)) 
-            filas.append(j)
+            filas.append(str(j))
         if len(linea_sin_sangria) >= 2 and (linea_sin_sangria[0] == "," or linea_sin_sangria[1] == ","):
             tokens.append(",") 
             columnas.append(str(i))
-            filas.append(j) 
-        for i in range(len(linea_sin_sangria)):
-            if (linea_sin_sangria[i] in ["@", "/", "#"]) and i == len(linea_sin_sangria) - 1:
-                errores.append(linea_sin_sangria[i])
+            filas.append(str(j)) 
+        if linea_sin_sangria[-1] not in [",", ";", "{", "}", "[", "]"]:
+            errores.append(linea_sin_sangria[-1:])
+            erFilas.append(str(j))
+            erColumnas.append(str(i)) 
         j+=1    
         i+=1 
-    print(errores)
-        
--- reporte.py
+++ reporte.py
@@ -1,9 +1,8 @@
 import webbrowser
 import os
-from analisisLexico import tokens,palabras_reservadas,columnas,filas
+from analisisLexico import tokens,palabras_reservadas,columnas,filas, erColumnas, erFilas,errores
 
 def reporte():
-    errores=[]
     # Definir el contenido de la tabla en formato HTML
     tabla_html = '''<!DOCTYPE html>
 <html lang="en">
@@ -12,7 +11,7 @@ def reporte():
     <title>Reporte</title>
 </head>
 <body>
-    <table border="1" cellborder="1" cellspacing="0">
+    <table border="1" cellborder="1" cellspacing="0" style="position: absolute; left: 0px;">
     <tr>
         <td>Token</td>
         <td>Lexema</td>
@@ -50,7 +49,27 @@ def reporte():
         tabla_html += '''    </tr>\n'''
     
     
-    tabla_html+='''</table>\n</body>\n</html>'''
+    tabla_html+='''</table>\n'''
+    
+    tabla_html+='''<table border="1" cellborder="1" cellspacing="0" style="position: absolute; left: 50%;">\n'''
+    tabla_html+='''    <tr>
+        <td>Token</td>
+        <td>Filas</td>
+        <td>Columna</td>           
+    </tr>\n'''
+    i=0
+    for i in range(len(errores)):
+        tabla_html+='    <tr>\n'
+        tabla_html+='       <td>'+errores[i]+'</td>\n'
+        tabla_html+='       <td>'+erFilas[i]+'</td>\n'
+        tabla_html+='       <td>'+erColumnas[i]+'</td>\n'
+        tabla_html+='    </tr>\n'     
+    tabla_html+='''</table>\n'''
+    
+    
+    
+    
+    tabla_html+='''</body>\n</html>'''
 
 
     # Agregar el contenido HTML en un archivo
